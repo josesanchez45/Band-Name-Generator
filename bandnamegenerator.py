9be208c5a02aa62378c4e50b5235574ce177265c
jose sanchez <jose.sanchez.artist@gmail.com>
2022-10-31 21:26:11 -0700

    Initial Commit

---
+++ bandnamegenerator.py
@@ -0,0 +1,12 @@
+#Create a greeting for your program
+print("Hello welcome to the band name generator")
+
+#Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+
+#Ask the user for the name of a pet
+pet = input("What is the name of your pet?\n")
+
+#Combine the name of their city and pet and show them their band name.
+print(f"Ok your band name is {city} {pet}\n")
+print("Awesome!!")
